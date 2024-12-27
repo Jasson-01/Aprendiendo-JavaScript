26863145732ddf95c50052c2f44aedc55adfb205
linuxlite <jason.davila001@gmail.com>
2024-12-27 10:19:32 -0300

    Agregando clase 4

---
+++ 4TipoDeDatos.js
@@ -0,0 +1,30 @@
+
+// NUMBER
+let a = 5 // valor entero
+let b = 3.5 // valor flotante
+
+// STRING
+let c = "cadena de texto" // valor string (texto)
+
+// BOOLEANO
+let d = true // Verdadero
+let e = false // False 
+
+// UNDEFINED
+let f // No se asigno valor por lo cual es indefinido
+
+// NUll
+let g = null;
+
+// OBJECT
+let objeto = {  // valor objeto
+    clave:variableNumero
+}
+
+// ARRAY
+let array1 = [1,2,3,4]
+let array2 = ["JavaScript","CSS","hTML"]
+
+// DATE
+const fecha = new Date("05-06-2000")
+
