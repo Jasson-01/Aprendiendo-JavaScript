99256253f5991a33367a7bc09e8fc50a3b392b8a
linuxlite <jason.davila001@gmail.com>
2024-12-27 10:36:38 -0300

    Agregando clase 5

---
+++ 5OperadoresLogicos.js
@@ -0,0 +1,26 @@
+
+let x = 4
+let y = "7"
+let z = 2
+
+// res es un booleano
+let sonIguales = x == y // res = True, Compara valores absolutos 
+let sonIguales2 = x === y // res = False, Compara valores absolutos y tipos
+
+// Operador Negacion
+let esDistinto = x != z // res = true
+
+// Mayor
+let m1 = x > z
+// Mayor o igual que
+let m11 = x >= z 
+
+// Menor
+let m2 = z < x
+
+// Menor o igual que
+let m22 = z <= x
+
+// && -> "Y logica"
+
+// || -> "O logica" 
