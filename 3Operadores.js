54b998b6897065af8497a52c7c2d7b0c02feca09
linuxlite <jason.davila001@gmail.com>
2024-12-27 10:18:27 -0300

    Agreganddo clase 2

---
+++ 3Operadores.js
@@ -0,0 +1,22 @@
+
+// Operadores:
+const variable = "valor de la variable" // Operador asignacion
+
+const suma = 1 + 3
+const resta = 1- 3 
+const multiplicacion = 3 * 4 // Multiplicacion
+const ddivision = 8 / 4 // Division
+const resto = 6 % 2  // Resto
+const exponente = 3 ** 3 // Exponenciacion
+
+// -----------------------------
+
+let x = 5;
+let y = 3;
+
+x++; // Aumentador
+y--; // Disminuidor
+
+// Para imprimir en la consola
+console.log(y) // res = 2
+console.log(x) // res = 6
