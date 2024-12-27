c740c6897bc75ff61316bad0a58011a45b00c0f6
linuxlite <jason.davila001@gmail.com>
2024-12-27 10:03:00 -0300

    Subiendo clase 2

---
+++ 2NumberyString.js
@@ -0,0 +1,10 @@
+
+// NUMEROS
+let variableNumero = 10  // valor numerico
+let varableDecimal = 6.5 // valor flotante
+
+// TEXTO
+
+let variableTexto = 'texto'  // valor String(cadena de caracteres)
+let variableTexto2 = "texto" // valor String(cadena de caracteres)
+let variableTexto3 = `Esta es una variable distinta ${variableNumero}`
