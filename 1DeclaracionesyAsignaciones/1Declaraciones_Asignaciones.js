b2a445efa0989ec614df005c64d677c0e6a08f09
linuxlite <jason.davila001@gmail.com>
2024-12-27 00:50:49 -0300

    Agregando clase1 - JavaScript - Declaraciones y Asignaciones

---
+++ 1DeclaracionesyAsignaciones/1Declaraciones_Asignaciones.js
@@ -0,0 +1,38 @@
+//.----------------------- Hay 3 formas de declarar una variable en JavaScript:
+
+// hay una que ya no se utiliza mas -> "var"
+
+//--------- 1era Forma
+var nombreDeLaVariable = "Pedro"
+
+//--------- 2da Forma
+let nombreDeVariable2 = "Jason"
+
+// 2.2
+let nombreDeVariable22 
+nombreDeVariable22 = "Aldayr"
+
+
+//--------- 3era Forma
+const nombreVariable3 = "constante"
+
+// -------------------------- Declarando variables al mismo tiempo:
+
+// Con let se puede declarar varias variables al mismo tiempo
+
+let x,y,z;
+
+x=5;
+y=7;
+z= x + y;
+
+// x=5;y=7;z=x+y ---> Tambien se puede hacer de esta forma
+
+// JavaScript no interpreta espacios y enters
+// Es opcional usar los ";"
+
+// Tambien podemos escribir de esta forma si el nombre de la variable es muy largo
+const datoQueVieneDeLaBaseDeDatosDeElCliente = 
+     "Pedro";
+
+
